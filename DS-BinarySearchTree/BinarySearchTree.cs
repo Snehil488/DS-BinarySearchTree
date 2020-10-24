39077ef1c184b5844d19a55849161e5091964b0c
Snehil <snehilverma488@gmail.com>
2020-10-24 20:50:02 +0530

    [Snehil] Add : Searching Given Element in Binary Search Tree

--- DS-BinarySearchTree/BinarySearchTree.cs
+++ DS-BinarySearchTree/BinarySearchTree.cs
@@ -27,6 +27,18 @@ namespace DS_BinarySearchTree
             }
             return treeSize;
         }
+        public int Search(int data)
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Binary Search Tree is Empty");
+                return 0;
+            }
+            else
+            {
+                return root.Search(data);
+            }
+        }
         public void Display()
         {
             if (root == null)
